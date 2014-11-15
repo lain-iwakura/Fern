66764f6c8ab8eb4d32c4eace4a0b4d7cf177398a
andreyk <andreykostin@inbox.ru>
2014-11-15 22:03:50 +0300

    [add] Types.hpp

---
+++ Fern/include/Fern/Types.hpp
@@ -0,0 +1,25 @@
+#ifndef INCLUDE_FERN_TYPES_HPP
+#define INCLUDE_FERN_TYPES_HPP
+
+#include <cstddef>
+
+namespace Fern{
+
+typedef double fnum; 
+
+typedef int num;
+
+typedef unsigned int unum;
+
+typedef std::size_t sznum;
+
+typedef num pxlnum;
+
+typedef fnum pxlfnum;
+
+typedef char clrnum;
+
+}
+
+
+#endif // <- INCLUDE_FERN_TYPES_HPP
